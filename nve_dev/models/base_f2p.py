eddad73286196730806bf5c6a5031a5cb8c36fd1
Mikhail Okunev <mishka.okunev@gmail.com>
2022-11-17 03:33:52 -0500

    Adding an option for unsigned distance function as well

--- nve_dev/models/base_f2p.py
+++ nve_dev/models/base_f2p.py
@@ -3,12 +3,14 @@ Baseline model for Features to point estimates.
 Model takes n features, and a point to return the (n-dimensional) field value.
 """
 
+import torch
 from torch import nn
 
 # Simple MLP model
 class FeatureToPoint(nn.Module):
-    def __init__(self, n_in, n_out, n_hidden, n_layers, activation=nn.ReLU):
+    def __init__(self, n_in, n_out, n_hidden, n_layers, activation=nn.ReLU, unsigned_df=False):
         super().__init__()
+        self.unsigned_df = unsigned_df
         self.layers = nn.ModuleList()
         self.layers.append(nn.Linear(n_in, n_hidden))
         self.layers.append(activation())
@@ -20,8 +22,10 @@ class FeatureToPoint(nn.Module):
     def forward(self, x):
         for layer in self.layers:
             x = layer(x)
+        if self.unsigned_df:
+            x = torch.abs(x)
         return x
 
 if __name__ == "__main__":
-    model = FeatureToPoint(8, 1, 64, 2)
+    model = FeatureToPoint(8, 1, 64, 2, unsigned_df=True)
     print(model)
