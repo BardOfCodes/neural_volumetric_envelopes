1f5d5ae50c044883a3c3302164b0763cf693e3b1
bardofcodes <adityaganeshan@gmail.com>
2022-11-21 14:57:09 -0500

    Pep8 + Skeleton

--- nve_dev/trainer.py
+++ nve_dev/trainer.py
@@ -7,3 +7,40 @@ A Trainer class, which:
 
 ## Eventually we can branch it out if need be.
 """
+
+class Trainer():
+    
+    def __init__(self, train_config):
+        pass
+        # Instantiate logger.
+    
+    def train(self, train_settings):
+        
+        model.train()
+        for epoch in range(n_epochs):
+            
+            for iteration_ind, batch in enumerate(dataloader):
+                
+                input_data = format_data(batch)
+                loss = self.calculate_loss(model, input_data)
+                
+                
+                optimizer.zero_grad(set_to_none=True)
+                loss.backward()
+                optimizer.step()
+                
+                self.log_training_details()
+            
+        
+        if (epoch + 1) % eval_epoch == 0:
+            # perform evaluation
+            model.eval()
+            self.perform_evaluation()
+            model.train()
+        
+        if (epoch + 1) % save_iter == 0:
+            self.checkpoint()
+
+
+
+                
