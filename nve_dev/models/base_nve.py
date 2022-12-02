a1c3d5855ff7f1c29616acf46e4ca772f2615a81
Marc Mapeke <marcmapeke@gmail.com>
2022-12-02 17:14:07 -0500

    some model changes

--- nve_dev/models/base_nve.py
+++ nve_dev/models/base_nve.py
@@ -6,17 +6,22 @@ import torch.nn as nn
 import torch as th
 from .base_e2f import PointNetLatent
 from .base_f2p import FeatureToPoint
-
+import numpy as np
+# import rff # pip install random-fourier-features-pytorch
 
 class NVEModel(nn.Module): 
     def __init__(self, model_config):
         super(NVEModel, self).__init__()
         self.e2f = PointNetLatent(model_config.E2F.INPUT_DIM, model_config.E2F.NUM_LATENTS,
-                                  model_config.E2F.LATENT_DIM, feature_transform=False, normalize_latents=False)
+                                  model_config.E2F.LATENT_DIM, model_config.E2F.FEATURE_TRANSFORM, normalize_latents=False)
 
         self.f2p = FeatureToPoint(model_config.F2P.INPUT_DIM, model_config.F2P.OUTPUT_DIM,
                                   model_config.F2P.HIDDEN_DIM, model_config.F2P.NUM_LAYERS)
+        
         # Add positional encoding to the points?
+        # self.positinal_encoding = rff.layers.PositionalEncoding(sigma=1.0, m=10)
+        self.positinal_encoding = None
+
         # Add Flatten
         self.flatten = nn.Flatten()
 
@@ -25,13 +30,11 @@ class NVEModel(nn.Module):
     # surface points, B X D
     @th.no_grad()
     def predict_sdf(self, sdf_points, surface_points) :  
-        # TODO: Double check this logic
-        # B X 8 X L
-        pointnet_output = self.e2f.forward(surface_points)
-        feats = pointnet_output[0]
         
+        # feats -B X NUM_LATENTS X LATENT_DIM
+        feats, _, _ = self.e2f.forward(surface_points)
+
         # Expand the features for point prediction
-        
         expanded_features = []
         sdf_points = [sdf_points]
         for ind, cur_points in enumerate(sdf_points):
@@ -41,6 +44,10 @@ class NVEModel(nn.Module):
         expanded_features = th.cat(expanded_features, 0)
         # B(E) X 3
         sdf_points = th.cat(sdf_points, 0)
+        
+        if self.positinal_encoding is not None :
+            sdf_points = self.positinal_encoding(sdf_points)
+        
         # Flatten and cat
         flattened_input = self.flatten(expanded_features)
         f2p_input = th.cat([flattened_input, sdf_points], 1)
@@ -51,9 +58,9 @@ class NVEModel(nn.Module):
         # B X D
         input_obj = input_data['surface_points']
         
-        # B X 8 X L
-        pointnet_output = self.e2f.forward(input_obj)
-        feats = pointnet_output[0]
+        # feats -B X NUM_LATENTS X LATENT_DIM
+        feats, _, trans_feat = self.e2f.forward(input_obj)
+
         # Expand the features for point prediction
         training_points = input_data['training_points']
         expanded_features = []
@@ -64,8 +71,13 @@ class NVEModel(nn.Module):
         expanded_features = th.cat(expanded_features, 0)
         # B(E) X 3
         training_points = th.cat(training_points, 0)
+
+        if self.positinal_encoding is not None :
+            training_points = self.positinal_encoding(training_points)
+
         # Flatten and cat
         flattened_input = self.flatten(expanded_features)
         f2p_input = th.cat([flattened_input, training_points], 1)
+
         pred_values = self.f2p.forward(f2p_input)
-        return pred_values
+        return pred_values, trans_feat
