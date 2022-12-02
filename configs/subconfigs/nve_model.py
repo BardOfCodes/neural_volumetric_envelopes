221dcb2eacbb47a1b77f8d70934a11da2194d94d
Marc Mapeke <marcmapeke@gmail.com>
2022-12-02 17:49:51 -0500

    pushing my params for overfitting 4 cubes

--- configs/subconfigs/nve_model.py
+++ configs/subconfigs/nve_model.py
@@ -11,5 +11,5 @@ config.E2F.FEATURE_TRANSFORM = True
 config.F2P = CN()
 config.F2P.INPUT_DIM = config.E2F.NUM_LATENTS * config.E2F.LATENT_DIM + 3
 config.F2P.OUTPUT_DIM = 1
-config.F2P.HIDDEN_DIM = 512
-config.F2P.NUM_LAYERS = 5
+config.F2P.HIDDEN_DIM = 256
+config.F2P.NUM_LAYERS = 2
