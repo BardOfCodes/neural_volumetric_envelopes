0c9ffa0bf08bd92a0adbfdf771898b63ba05b973
bardofcodes <adityaganeshan@gmail.com>
2023-10-16 10:41:51 -0400

    merge fix

--- nve_dev/utils/generic_utils.py
+++ nve_dev/utils/generic_utils.py
@@ -8,6 +8,8 @@ arg_parser.add_argument('--config-file', type=str,
                         default="../configs/baseline.py")
 arg_parser.add_argument('--debug', action='store_true',
                         help="Enables DEBUG mode; for visualizer computes mesh independetly per envelope")
+arg_parser.add_argument('--save-loc', type=str,
+                        default="../results/multi_shape_32")
 
 
 def load_config(args):
